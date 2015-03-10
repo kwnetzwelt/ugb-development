53c010662bccccd4d1637acdba244aa7a1b4c1fd
Kai Wegner <kwnetzwelt@gmail.com>
2015-03-10 20:01:19 +0100

    added test file and updated commit pointer for ugb

---
+++ Assets/Test.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityGameBase;
+
+public class Test : MonoBehaviour {
+
+    TouchInformation touch;
+
+	void Start () {
+        UGB.Input.TouchStart += Input_TouchStart;
+	}
+
+    private void Input_TouchStart(TouchInformation touchInfo)
+    {
+        Debug.Log("Touch was started");
+        touch = touchInfo;
+    }
+
+    void Update()
+    {
+        if(touch != null)
+        {
+            Debug.Log("Touch active");
+
+            if(touch.IsDead)
+            {
+                Debug.Log("Touch died");
+                touch = null;
+            }
+    }
+}
